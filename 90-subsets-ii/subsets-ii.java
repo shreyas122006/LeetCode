aac8087ca5ebfa4ad3057d6dce6287241c677621
Shreyash Chavan <shreyash7122006@gmail.com>
2026-09-05 20:47:00 +0530

    Time: 3 ms (54.28%) | Memory: 44.9 MB (88.63%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,9 +1,7 @@
 class Solution {
     List<List<Integer>> store = new ArrayList<>();
     private void BackTrack(int nums[], int idx, ArrayList<Integer> curr) {
-        if(!store.contains(curr)) {
-            store.add(new ArrayList<>(curr));
-        } 
+        store.add(new ArrayList<>(curr));
         for(int i=idx; i<nums.length; i++) {
             if( i > idx && nums[i] == nums[i-1]) {
                 continue;
