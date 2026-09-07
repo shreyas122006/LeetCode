ca02ebfacf83c4c5eaf1c3d6ef24e2cfaf72c359
Shreyash Chavan <shreyash7122006@gmail.com>
2026-09-07 09:11:07 +0530

    Time: 2 ms (97.38%) | Memory: 45 MB (40.50%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,25 +1,43 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        // HashSet<Integer> set = new HashSet<>();
+        // int i=0;
+        // int j=0;
+        // while (i<nums1.length && j<nums2.length) {
+        //     if(nums1[i] == nums2[j]) {
+        //         set.add(nums1[i]);
+        //         i++;
+        //         j++;
+        //     } else if(nums1[i] > nums2[j]) {
+        //         j++;
+        //     } else {
+        //         i++;
+        //     }
+        // }
+        // int store[] = new int[set.size()];
+        // int idx = 0;
+        // for(int x : set) {
+        //     store[idx] = x;
+        //     idx++;
+        // }
+        // return store;
         HashSet<Integer> set = new HashSet<>();
-        int i=0;
-        int j=0;
-        while (i<nums1.length && j<nums2.length) {
-            if(nums1[i] == nums2[j]) {
-                set.add(nums1[i]);
-                i++;
-                j++;
-            } else if(nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                i++;
+        for(int x : nums1) {
+            set.add(x);
+        }
+        HashSet<Integer> result = new HashSet<>();
+        for(int y : nums2) {
+            if(set.contains(y)) {
+                result.add(y);
             }
         }
-        int store[] = new int[set.size()];
+        int store[] = new int[result.size()];
+
         int idx = 0;
-        for(int x : set) {
-            store[idx] = x;
+        for(int z : result) {
+            store[idx] = z;
             idx++;
         }
         return store;
