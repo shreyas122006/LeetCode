ed4f92c9f1e15e5eb2aee983005a8fea9e23ac93
Shreyash Chavan <shreyash7122006@gmail.com>
2026-09-09 19:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (43.54%) - LeetSync

--- 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.java
+++ 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.java
@@ -1,17 +1,16 @@
 class Solution {
     public String categorizeBox(int length, int width, int height, int mass) {
-        long volume = (long) length*width*height;
-        StringBuilder sb = new StringBuilder("Neither");
-        if((length >= 10000 || width >= 10000 || height >= 10000 || volume >= 1000000000) && mass >= 100) {
-            sb.setLength(0);
-            sb.append("Both");
-        } else if(length >= 10000 || width >= 10000 || height >= 10000 || volume >= 1000000000) {
-            sb.setLength(0);
-            sb.append("Bulky");
-        } else if(mass >= 100) {
-            sb.setLength(0);
-            sb.append("Heavy");
+        long volume = (long) length * width * height;
+        boolean bulky = length >= 10000 || width >= 10000 || height >= 10000 || volume >= 1000000000;
+        boolean heavy = mass >= 100;
+        if(bulky && heavy) {
+            return "Both";
+        } else if(bulky) {
+            return "Bulky";
+        } else if(heavy) {
+            return "Heavy";
+        } else {
+            return "Neither";
         }
-        return sb.toString();
     }
 }
