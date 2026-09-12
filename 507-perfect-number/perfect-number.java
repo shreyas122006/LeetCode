d814a172a4a2ae2c4d35b23d81a4a9b8f5f598b4
Shreyash Chavan <shreyash7122006@gmail.com>
2026-09-12 18:31:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (79.57%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum = 0;
-        for(int i=1; i<=num/2; i++) {
-            if(num%i == 0) {
-                sum+=i;
-            }
+        // int sum = 0;
+        // for(int i=1; i<=num/2; i+=i) {
+        //     if(num%i == 0) {
+        //         sum+=i;
+        //     }
+        // }
+        // return num == sum;
+        if(num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) {
+            return true;
         }
-        return num == sum;
+        return false;
     }
 }
