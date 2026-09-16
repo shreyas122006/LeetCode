a14070aa7fc6e8a848af3c6d3be523fe37d2c32f
Shreyash Chavan <shreyash7122006@gmail.com>
2026-09-16 21:51:55 +0530

    Time: 1 ms (47.99%) | Memory: 44.7 MB (84.18%) - LeetSync

---
+++ 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countTestedDevices(int[] batteryPercentages) {
+        int count = 0;
+        for(int i=0; i<batteryPercentages.length; i++) {
+            if(batteryPercentages[i]>0) {
+                count++;
+                for(int j=i+1; j<batteryPercentages.length; j++) {
+                    if(batteryPercentages[j] != 0) {
+                        batteryPercentages[j]--;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
