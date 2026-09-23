1962b168a94b0db9e82419cb385bd5b204502aff
Shreyash Chavan <shreyash7122006@gmail.com>
2026-09-24 00:07:59 +0530

    Time: 1 ms (98.38%) | Memory: 42.7 MB (54.74%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.java
+++ 4321-digit-frequency-score/digit-frequency-score.java
@@ -1,15 +1,9 @@
 class Solution {
     public int digitFrequencyScore(int n) {
         int score = 0;
-        int digit = 1;
-        HashMap<Integer,Integer> map = new HashMap<>();
         while(n>0) {
-            digit = n%10;
-            map.put(digit,map.getOrDefault(digit,0)+1);
+            score += n%10;
             n /= 10;
-        }
-        for(int key : map.keySet()) {
-            score += key * map.get(key);
         }
         return score;
     }
