454f33a92cd5bb87789df6d0b4875202be1c9dbb
Shreyash Chavan <shreyash7122006@gmail.com>
2026-09-25 21:19:31 +0530

    Time: 2 ms (99.73%) | Memory: 46.4 MB (77.38%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.java
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.java
@@ -1,18 +1,35 @@
+// class Solution {
+//     public List<Integer> intersection(int[][] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         ArrayList<Integer> store = new ArrayList<>();
+//         for(int i=0; i<nums.length; i++) {
+//             for(int j=0; j<nums[i].length; j++) {
+//                 map.put(nums[i][j],map.getOrDefault(nums[i][j],0)+1);
+//             }
+//         }
+//         for(int key : map.keySet()) {
+//             if(map.get(key) == nums.length) {
+//                 store.add(key);
+//             }
+//         }
+//         Collections.sort(store);
+//         return store;
+//     }
+// }
 class Solution {
     public List<Integer> intersection(int[][] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        ArrayList<Integer> store = new ArrayList<>();
-        for(int i=0; i<nums.length; i++) {
-            for(int j=0; j<nums[i].length; j++) {
-                map.put(nums[i][j],map.getOrDefault(nums[i][j],0)+1);
+        int[] freq=new int[1001];
+        for(int[] arr:nums) {
+            for(int x:arr) {
+                freq[x]++;
             }
         }
-        for(int key : map.keySet()) {
-            if(map.get(key) == nums.length) {
-                store.add(key);
+        List<Integer> res=new ArrayList<>();
+        for(int i=1;i<=1000;i++) {
+            if(freq[i]==nums.length) {
+                res.add(i);
             }
         }
-        Collections.sort(store);
-        return store;
+        return res;
     }
 }
